5539e59cb4a80abd5e3980d9911bd537d7694a2c
artfulKraken <artfulKraken@proton.me>
2025-04-15 09:26:44 -0700

    added example_.gasScraperConfig.yml, functions for getting csv data and writing it, changed internal data type for gas prices to float on get data functions.

--- Gas_Price_Scraper.py
+++ Gas_Price_Scraper.py
@@ -8,11 +8,61 @@ from operator import itemgetter
 import requests
 import certifi
 import urllib3
-
-
+import yaml
 
 
 ###############################    FUNCTIONS    ##########################################
+def importConfig():
+  # imports yaml config file
+  home_dir = os.path.expanduser('~')
+  with open(home_dir + "/.gasScraperConfig.yml", "r") as file:
+    config = yaml.safe_load(file)
+  return config
+
+def get_existing_csv_data(filepath):
+  if os.path.isfile(filepath):
+    with open(filepath, 'r') as file:
+      lstFile= file.readlines()
+      file.close()
+    existGasData = []
+    i = 0
+    existGasDataHeaderRow = lstFile.pop(0)
+    for line in lstFile:
+      i+=1
+      dictData = line.strip("\n").split(",")
+      existGasData.append( {
+        "date": dictData[0],
+        "MetroArea": dictData[1],
+        "regular": float( dictData[2].replace( '$', '' ) ),
+        "mid": float( dictData[3].replace( '$', '' ) ),
+        "premium": float( dictData[4].replace( '$', '' ) ),
+        "dataAcc":int( dictData[5] ) 
+      } )
+    existGasData.sort(key=itemgetter("date","MetroArea"))
+    lastDate = DT.date.fromisoformat(existGasData[-1]["date"])
+    return ( existGasData, lastDate )
+
+  else:
+    ## If file doesnt exist, set lastDate to 7 days ago to use as a starting point for collecting data and existing data to empty list.
+    lastDate = DT.date.today() - DT.timedelta(days=7) 
+    existGasData = []  
+    return ( existGasData, lastDate )
+
+def insertCsvData( existGasData, lstCAMetroPricing, filepath ):
+    # rewrites csv file with existing and new data
+    try:
+      existGasData.extend(lstCAMetroPricing)
+    except:
+      existGasData = []
+      existGasData.extend(lstCAMetroPricing)
+
+    f = open(filepath, "w")
+    f.write(existGasDataHeaderRow)
+    for dataline in existGasData:
+      f.write(",".join([dataline["date"],dataline["MetroArea"],dataline["regular"],dataline["mid"],dataline["premium"],dataline["dataAcc"]])+"\n")
+    f.close()
+
+
 def metroAreaGasPrice(MetroAreaHeaderSoup):
     MetroArea = MetroAreaHeaderSoup.next_sibling
     while MetroArea.name != "div":
@@ -115,11 +165,27 @@ def extrapGasData(metroAreaData,curDate,lastDate):
 
 
 ###############################  End FUNCTIONS  ##########################################
-filepath = "/Users/daveboggs/Library/CloudStorage/OneDrive-Personal/40 Via Gatillo/_EV_Data/Gas_Price_Data/Gas_Price_Data.csv"
-GasPriceURL = "https://gasprices.aaa.com/?state=CA#state-metro"
 
-#requestSite = Request(GasPriceURL,headers={'User-Agent': 'Mozilla/5.0'})
-#html = requests.get(GasPriceURL, verify=certifi.where())
+
+
+#import yaml config file
+config = importConfig()
+
+GasPriceURL = config["general"]["GasPriceURL"]
+
+#### get existing data from csv or most recent data from mysql, depending on config settings.  exit if incorrect output value (not csv or mysql)
+
+if config["general"]["output"] == 'mysql':
+  lastDate = getLastMySqlData(  )
+elif config["general"]["output"] == 'csv':
+  tupLastData = get_existing_csv_data( config["csv"]["filepath"] )
+  existGasData = tupLastData[0]
+  lastDate = tupLastData[1]
+
+else:
+  print('Invalid Config "output" option.  Program exiting')
+  exit(1)
+
 
 http = urllib3.PoolManager(
     cert_reqs="CERT_REQUIRED",
@@ -132,8 +198,7 @@ resp = http.request("GET", GasPriceURL,headers=headers)
 #html = urlopen(resp).read()
 soup = BeautifulSoup(resp.data,'html.parser')
 
-#use current date until add code to scrape date of gas price data
-#curDate = DT.date.today()
+
 dateSoup = soup.find("p",class_="price-text price-text--blue").next_sibling
 while dateSoup.name !="p":
     dateSoup = dateSoup.next_sibling
@@ -142,48 +207,20 @@ dateString = dateString.split(" ")[-1].split("/")
 
 curDate = DT.date(int("20"+dateString[2]),int(dateString[0]),int(dateString[1]))
 
-if os.path.isfile(filepath):
-        with open(filepath, 'r') as file:
-            lstFile= file.readlines()
-            file.close()
-        existGasData = []
-        i = 0
-        existGasDataHeaderRow = lstFile.pop(0)
-        for line in lstFile:
-            i+=1
-            dictData = line.strip("\n").split(",")
-            existGasData.append({
-                "date": dictData[0],
-                "MetroArea": dictData[1],
-                "regular": dictData[2],
-                "mid": dictData[3],
-                "premium": dictData[4],
-                "dataAcc":dictData[5]})
-        existGasData.sort(key=itemgetter("date","MetroArea"))
-        lastDate = DT.date.fromisoformat(existGasData[-1]["date"])            
-else:
-    ## If file doesnt exist, set lastDate to 7 days ago to use as a starting point for collecting daa.
-    lastDate = DT.date.today() - DT.timedelta(days=7)  
 
 if curDate != lastDate:
-    lstCAMetroPricing =[]
-    MetroAccordian = soup.find_all(attrs={"class": "accordion-prices metros-js"})
-    MetroAreas = MetroAccordian[0].find_all("h3")
-    for h3 in MetroAreas:
-        #if h3.next == "Orange County":
-        metroAreaData = metroAreaGasPrice(h3)
-        lstCAMetroPricing.extend(extrapGasData(metroAreaData,curDate,lastDate))
+  lstCAMetroPricing =[]
+  MetroAccordian = soup.find_all(attrs={"class": "accordion-prices metros-js"})
+  MetroAreas = MetroAccordian[0].find_all("h3")
+  for h3 in MetroAreas:
+    #if h3.next == "Orange County":
+    metroAreaData = metroAreaGasPrice(h3)
+    lstCAMetroPricing.extend(extrapGasData(metroAreaData,curDate,lastDate))
 
 
-    lstCAMetroPricing.sort(key=itemgetter("date","MetroArea"))
-    try:
-        existGasData.extend(lstCAMetroPricing)
-    except:
-        existGasData = []
-        existGasData.extend(lstCAMetroPricing)
+  lstCAMetroPricing.sort(key=itemgetter("date","MetroArea"))
 
-    f = open(filepath, "w")
-    f.write(existGasDataHeaderRow)
-    for dataline in existGasData:
-        f.write(",".join([dataline["date"],dataline["MetroArea"],dataline["regular"],dataline["mid"],dataline["premium"],dataline["dataAcc"]])+"\n")
-    f.close()
+  if config["general"]["output"] == 'mysql':
+    insertMySqlData(  )
+  elif config["general"]["output"] == 'csv':
+    insertCsvData( existGasData, lstCAMetroPricing, config["csv"]["filepath"] )
